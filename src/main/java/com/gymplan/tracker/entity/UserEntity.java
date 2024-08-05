d49f3dc2721bbd7fa0b07299c5a66325dcfbbf46
Pouria Norouzi <pourianrzi@gmail.com>
2024-08-05 13:12:47 +0200

    feat: add user entity

---
+++ src/main/java/com/gymplan/tracker/entity/UserEntity.java
@@ -0,0 +1,39 @@
+package com.gymplan.tracker.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(unique = true)
+    private String username;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String fullName;
+
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @Size(max = 12)
+    private String phoneNumber;
+
+}
