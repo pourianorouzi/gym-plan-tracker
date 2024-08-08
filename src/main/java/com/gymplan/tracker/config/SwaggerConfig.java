e759d7e31a1021d66f407dcc516ccefe0cc0d444
Pouria Norouzi <pourianrzi@gmail.com>
2024-08-08 21:55:31 +0200

    feat: add swagger

---
+++ src/main/java/com/gymplan/tracker/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.gymplan.tracker.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Gym Plan Tracker")
+                        .version("1.0.0")
+                        .description("Detailed description of Gym Plan Tracker API")
+                        .contact(new io.swagger.v3.oas.models.info.Contact()
+                                .name("Pouria Norouzi")
+                        )
+                );
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+
+}
